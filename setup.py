84fc7fa3fc4ba335ad499b5d80c122cd9209ec00
Lorenzo Bocchi <lorenzobocchi99@yahoo.com>
2023-10-06 21:54:13 +0200

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="ntscraper",
-    version="0.2.1",
+    version="0.2.2",
     description="Unofficial library to scrape Twitter profiles and posts from Nitter instances",
     long_description=long_description,
     long_description_content_type="text/markdown",
